20010b4b6be2ee052cff6e230060ccbbc2e30106
Johan <johan.iversen@gmail.com>
2015-04-14 16:44:21 +0200

    fixed gitignore

---
+++ desktop/src/com/jupiter/rogue/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jupiter.rogue.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jupiter.rogue.Rogue;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Rogue(), config);
+	}
+}
