30bb12d0cbbef1df4566b2c1333549fa9460698e
Carlshede <carlshede@gmail.com>
2015-04-16 10:46:12 +0200

    added dummy files in view/controller

---
+++ core/src/com/jupiter/rogue/Controller/Controller.java
@@ -0,0 +1,7 @@
+package com.jupiter.rogue.Controller;
+
+/**
+ * Created by Johan on 16/04/15.
+ */
+public class Controller {
+}
--- core/src/com/jupiter/rogue/Model/Hero.java
+++ core/src/com/jupiter/rogue/Model/Hero.java
@@ -4,4 +4,5 @@ package com.jupiter.rogue.Model;
  * Created by Johan on 16/04/15.
  */
 public class Hero {
+
 }
