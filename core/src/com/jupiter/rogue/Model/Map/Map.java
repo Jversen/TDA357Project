b2b91d51d181160d3e37264f9bd4b74d4f7ca9b1
Johannes <johannes.hilden@gmail.com>
2015-04-16 15:53:22 +0200

    La till karta

--- core/src/com/jupiter/rogue/Model/Map/Map.java
+++ core/src/com/jupiter/rogue/Model/Map/Map.java
@@ -1,8 +1,12 @@
 package com.jupiter.rogue.Model.Map;
 
+import java.util.ArrayList;
+
 /**
  * Created by Johan on 16/04/15.
  */
 public class Map {
 
+    private ArrayList<Room> rooms;
+    private
 }
--- core/src/com/jupiter/rogue/Model/Map/Room.java
+++ core/src/com/jupiter/rogue/Model/Map/Room.java
@@ -3,5 +3,19 @@ package com.jupiter.rogue.Model.Map;
 /**
  * Created by hilden on 2015-04-16.
  */
+
 public class Room {
+
+    private int sizeX;
+    private int sizeY;
+
+    public Room(int sizeX,int sizeY) {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+    }
+
+    public Room() {
+        
+    }
+
 }
