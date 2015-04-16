9dd812fcb954e5db30b306f87504df49b7a59787
Johan <johan.iversen@gmail.com>
2015-04-16 10:58:55 +0200

    created Position class

---
+++ core/src/com/jupiter/rogue/Model/Position.java
@@ -0,0 +1,42 @@
+package com.jupiter.rogue.Model;
+
+/**
+ * Created by Johan on 2015-04-16.
+ */
+public class Position {
+
+    private int xPos;
+    private int yPos;
+
+    public Position(){
+        this.xPos = 0;
+        this.yPos = 0;
+    }
+
+    public Position(int xPos, int yPos){
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public int getXPos() {
+        return xPos;
+    }
+
+    public int getYPos() {
+        return yPos;
+    }
+
+    public void setXPos(int yPos) {
+        this.xPos = yPos;
+    }
+
+    public void setYPos(int xPos) {
+        this.yPos = xPos;
+    }
+
+    public void changePosition(int deltaX, int deltaY){
+        this.xPos += deltaX;
+        this.yPos += deltaY;
+    }
+
+}
