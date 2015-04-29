c11401e897d3c26edab9bc801e91ad48ec56bb8b
Johannes <johannes.hilden@gmail.com>
2015-04-30 00:17:36 +0200

    Added the new running animation, for some reason ther spritesheet is in the background?.

--- desktop/src/com/jupiter/rogue/desktop/DesktopLauncher.java
+++ desktop/src/com/jupiter/rogue/desktop/DesktopLauncher.java
@@ -1,11 +1,16 @@
 package com.jupiter.rogue.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.jupiter.rogue.Rogue;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+
+		//TexturePacker.process("Data//HeroRunning", "Data//HeroRunning", "HeroRunningRight");  //Göra Atlasfil
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = 384;
         config.height = 216;
