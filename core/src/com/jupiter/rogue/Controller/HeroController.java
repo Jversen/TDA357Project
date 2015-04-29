1699329e80b0939c27402158ac0e2b1924f2f455
Johannes <johannes.hilden@gmail.com>
2015-04-29 22:08:10 +0200

    Added a still hero and a spritesheet for running, only as .png so far though.

--- core/src/com/jupiter/rogue/Controller/HeroController.java
+++ core/src/com/jupiter/rogue/Controller/HeroController.java
@@ -56,7 +56,7 @@ public class HeroController {
 
 
         //creates a sensor at the players feet
-        shape.setAsBox(10 / PPM, 1 / PPM, new Vector2(0, -18 / PPM), 0);
+        shape.setAsBox(8 / PPM, 1 / PPM, new Vector2(0, -18 / PPM), 0);
 
         FixtureDef feetSensorFixtureDef = new FixtureDef();
         feetSensorFixtureDef.shape = shape;
