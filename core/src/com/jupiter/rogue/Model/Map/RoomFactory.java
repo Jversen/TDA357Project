77b529923244cb6e55014fd295a65f3f3925e63d
Carlshede <carlshede@gmail.com>
2015-04-29 22:44:07 +0200

    added some more sroom-swapping stuff

--- core/src/com/jupiter/rogue/Model/Map/RoomFactory.java
+++ core/src/com/jupiter/rogue/Model/Map/RoomFactory.java
@@ -1,14 +1,34 @@
 package com.jupiter.rogue.Model.Map;
 
+import java.util.Random;
+
 /**
  * Created by Johan on 27/04/15.
  */
 public class RoomFactory {
+
     public Room getRoom(int roomNumber) {
         switch(roomNumber) {
             //case 1: return new Room("Rooms/BoxRoom.tmx");
             //case 2: return new Room("Rooms/BoxRoom2.tmx");
             default: return new Room();
         }
+    }
+
+    public Room getRandomRoom() {
+        Random rand = new Random();
+        return getRoom(rand.nextInt()%50);
+    }
+
+    /**
+     *
+     * @param width number of cells in width
+     * @param height number of cells in height
+     * @param entrance entrance to room from previous room
+     * @param exit whether or not the room should have another exit
+     * @return
+     */
+    public Room getRoom(int width, int height, DoorPlacement entrance, boolean exit) {
+        return new Room();
     }
 }
