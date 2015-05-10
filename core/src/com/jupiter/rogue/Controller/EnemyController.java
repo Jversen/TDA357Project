0d459f63e1d034c7260988e942e9a6aa063722c4
Johan Iversen <johan.iversen@gmail.com>
2015-05-10 15:40:27 +0200

    Created EnemyFactory class

---
+++ core/src/com/jupiter/rogue/Controller/EnemyController.java
@@ -0,0 +1,13 @@
+package com.jupiter.rogue.Controller;
+
+import com.jupiter.rogue.Model.Creatures.Enemy;
+import com.jupiter.rogue.View.EnemyView;
+
+/**
+ * Created by Johan on 2015-05-10.
+ */
+public class EnemyController {
+
+    EnemyView enemyView;
+    Enemy enemy;
+}
